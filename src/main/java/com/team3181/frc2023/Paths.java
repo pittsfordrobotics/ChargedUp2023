4e8feed8329cf417cce2eaee3cbcd73059011dd0
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2023-01-08 19:39:22 -0500

    added even more paths

--- src/main/java/com/team3181/frc2023/Paths.java
+++ src/main/java/com/team3181/frc2023/Paths.java
@@ -4,10 +4,12 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.team3181.frc2023.Constants.AutoConstants;
 
+import java.util.List;
+
 public class Paths {
-    public static final PathPlannerTrajectory TEST = PathPlanner.loadPath("Test", AutoConstants.MAX_SPEED);
-
     public static final PathPlannerTrajectory DROP_CLIMB_HIGH = PathPlanner.loadPath("Drop and Climb High", AutoConstants.MAX_SPEED);
-
     public static final PathPlannerTrajectory DROP_CLIMB_LOW = PathPlanner.loadPath("Drop and Climb Low", AutoConstants.MAX_SPEED);
+
+    public static final List<PathPlannerTrajectory> PIECE_AUTO_HIGH = PathPlanner.loadPathGroup("Drop and Climb High", AutoConstants.MAX_SPEED);
+    public static final List<PathPlannerTrajectory>  PIECE_AUTO_LOW = PathPlanner.loadPathGroup("Drop and Climb Low", AutoConstants.MAX_SPEED);
 }
---
+++ src/main/java/com/team3181/frc2023/commands/AutoCollectAndGo.java
@@ -0,0 +1,13 @@
+package com.team3181.frc2023.commands;
+
+
+import com.team3181.frc2023.Paths;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class AutoCollectAndGo extends SequentialCommandGroup {
+    public AutoCollectAndGo() {
+        super(
+            new SwervePathing(Paths.PIECE_AUTO_HIGH.get(0), true);
+        );
+    }
+}
