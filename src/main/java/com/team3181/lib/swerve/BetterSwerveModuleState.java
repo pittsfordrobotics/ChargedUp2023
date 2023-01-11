48f68eb9e1f0c4b0bde669177ff6b53a877e670d
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2023-01-10 23:09:35 -0500

    cleanup

--- src/main/java/com/team3181/lib/swerve/BetterSwerveModuleState.java
+++ src/main/java/com/team3181/lib/swerve/BetterSwerveModuleState.java
@@ -4,15 +4,14 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class BetterSwerveModuleState extends SwerveModuleState {
-    public double speedMetersPerSecond;
 
     /** Rad per sec */
     public double omegaRadPerSecond = 0;
 
-    public Rotation2d angle = Rotation2d.fromDegrees(0);
-
     /** Constructs a SwerveModuleState with zeros for speed and angle. */
-    public BetterSwerveModuleState() {}
+    public BetterSwerveModuleState() {
+        super();
+    }
 
     /**
      * Constructs a SwerveModuleState.
@@ -22,8 +21,7 @@ public class BetterSwerveModuleState extends SwerveModuleState {
      * @param omegaRadPerSecond The angular velocity of the module.
      */
     public BetterSwerveModuleState(double speedMetersPerSecond, Rotation2d angle, double omegaRadPerSecond) {
-        this.speedMetersPerSecond = speedMetersPerSecond;
-        this.angle = angle;
+        super(speedMetersPerSecond, angle);
         this.omegaRadPerSecond = omegaRadPerSecond;
     }
 }
