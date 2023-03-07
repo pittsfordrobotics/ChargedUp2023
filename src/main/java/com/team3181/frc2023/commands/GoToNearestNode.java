46858b3e08e5cd0ea1858000329365ea68692b9e
clmazzac <mazzacane.cam@gmail.com>
2023-03-06 20:40:29 -0500

    go to nearest node command

---
+++ src/main/java/com/team3181/frc2023/commands/GoToNearestNode.java
@@ -0,0 +1,34 @@
+package com.team3181.frc2023.commands;
+
+import com.team3181.frc2023.FieldConstants;
+import com.team3181.frc2023.subsystems.objectivetracker.ObjectiveTracker;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
+public class GoToNearestNode extends CommandBase {
+
+    public GoToNearestNode() {
+        addRequirements();
+    }
+
+    @Override
+    public void initialize() {
+        new SwervePathingOnTheFly(FieldConstants.AutoDrivePoints.nodeSelector(ObjectiveTracker.getInstance().getObjective().nodeRow));
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        // TODO: Make this return true when this Command no longer needs to run execute()
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+}
