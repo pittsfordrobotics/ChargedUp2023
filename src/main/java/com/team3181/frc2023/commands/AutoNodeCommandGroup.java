90602240e30f2cc4b2fb9a8b3b60520b927dda01
clmazzac <mazzacane.cam@gmail.com>
2023-03-10 15:46:14 -0500

    Fixed arctan (other stuff still doesnt work for red side)

---
+++ src/main/java/com/team3181/frc2023/commands/AutoNodeCommandGroup.java
@@ -0,0 +1,14 @@
+package com.team3181.frc2023.commands;
+
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public  class AutoNodeCommandGroup extends SequentialCommandGroup
+{
+     public AutoNodeCommandGroup()
+    {
+        // TODO: Add your sequential commands in the super() call, e.g.
+        //           super(new OpenClawCommand(), new MoveArmCommand());
+                super();
+    }
+}
