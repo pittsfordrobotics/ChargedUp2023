ab2b60fe2d3f3f80c27b3b067c0b90ae978f93e7
Eric Fletcher <eric@thefletcher.net>
2023-03-12 17:35:58 -0400

    Adjusting high peg height and fixing typo

--- src/main/java/com/team3181/frc2023/subsystems/Superstructure.java
+++ src/main/java/com/team3181/frc2023/subsystems/Superstructure.java
@@ -159,7 +159,7 @@ public class Superstructure extends SubsystemBase {
 //                        }
                         break;
                     case HIGH:
-                        fourBar.setRotations(new Rotation2d[]{ArmPositions.MID_CONE_SHOULDER, ArmPositions.MID_CONE_ELBOW}, false);
+                        fourBar.setRotations(new Rotation2d[]{ArmPositions.HIGH_CONE_SHOULDER, ArmPositions.HIGH_CONE_ELBOW}, false);
 //                        if (gamePieceLocal == GamePiece.CONE) {
 //                            fourBar.setRotations(new Rotation2d[]{ArmPositions.HIGH_CONE_SHOULDER, ArmPositions.HIGH_CONE_ELBOW}, false);
 ////                            fourBar.setRotations(fourBar.solve(SuperstructureConstants.ArmPositions.HIGH_CONE, true,true), false);
